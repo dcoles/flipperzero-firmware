5183eeb9ceb93a89c12344e55922b1267fee8800
David Coles <coles.david@gmail.com>
2022-09-24 01:45:57 -0700

    serial_cli: Reuse the host interpreter
    In some setups, `python3` will not be the same interpreter as which
    launched the script.

--- scripts/serial_cli.py
+++ scripts/serial_cli.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python3
+
 import logging
+import sys
 import subprocess
 from flipper.utils.cdc import resolve_port
 
@@ -7,7 +10,8 @@ def main():
     logger = logging.getLogger()
     if not (port := resolve_port(logger, "auto")):
         return 1
-    subprocess.call(["python3", "-m", "serial.tools.miniterm", "--raw", port, "230400"])
+
+    subprocess.call([sys.executable, "-m", "serial.tools.miniterm", "--raw", port, "230400"])
 
 
 if __name__ == "__main__":
